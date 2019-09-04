66969c06f9872ab5b00725963d23e3618915690d
PramodJana <Pramodjana395@gmail.com>
2019-09-04 10:39:00 +0530

    header modified

--- header8.php
+++ header8.php
@@ -6,12 +6,27 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-  </head>
+    
+    <!--Animate Css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+           <!--Wow JS-->
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js"></script>
+
+
+          <script>
+               new WOW().init();
+          </script>
+    <style>
+    .navbar-brand
+    {
+    animation-iteration-count:infinite;
+    }
+    </style>
+      </head>
   <body>
       <?php
       echo '<nav class="navbar navbar-expand-lg navbar-dark " style="background-color:#47A64A;">
-  <a class="navbar-brand" href="index.php">Cloud Is Everywhere</a>
+  <a class="navbar-brand wow pulse" href="index.php">Cloud Is Everywhere</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
